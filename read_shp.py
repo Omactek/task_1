5ebc937ea77df2c61a538808d83c58b8aa23026e
aktaz-fesoj <josefvicko@gmail.com>
2025-03-15 00:50:20 +0100

    comments

--- read_shp.py
+++ read_shp.py
@@ -8,21 +8,22 @@ def load_shapefile(data_path):
 
     qpolygons_list = []
 
+    #finding min and max coordinates (for drawing)
     min_x, min_y, max_x, max_y = data.total_bounds
-    #iterate through polygons in data, creating QPolygonFs, finding min and max coordinates (for drawing)
+    #iterate through polygons in data, creating QPolygonFs
     for i in range(0,len(data)):
         polygon = data.get_geometry(0)[i]
-        #print(data.get_geometry(0)[i])
         
         q_polygon = QPolygonF()
         
-        #iterate throught bounding points of a polygon, saving them in QPolygonF
+        #iterate throught bounding points of a polygon, saving them in QPolygonF, 
+        # rescaling the coordinates to make the polygons visible in the canvas
         for x,y in polygon.exterior.coords:
             x = (x - min_x) * 0.1
             y = (max_y - y) * 0.1
             point = QPointF(x, y)
             q_polygon.append(point)
-        qpolygons_list.append(q_polygon)    #append created polygon to the list of polygon
+        qpolygons_list.append(q_polygon)    #append created polygon to the list of polygons
     
     return qpolygons_list
         
